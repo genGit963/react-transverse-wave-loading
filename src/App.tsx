288c187429e4293efe5ed3530049426294e47586
Mahesh Bogati <117003503+genGit963@users.noreply.github.com>
2024-05-15 13:31:27 +0545

    Buttong Atomic Component is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { HeroButton } from "./components";
 
 function App() {
   return (
     <div className="App">
       <h1>Working to create Hero button</h1>
+      <HeroButton className="bg-red-400" variant="default" size="lg">Here Button</HeroButton>
     </div>
   );
 }
